334831dcdcf8824d1a5e85412e97c8a5f6a41586
maverich7465 <137097692+Il-MioNome@users.noreply.github.com>
2026-02-27 13:25:50 +0100

    Delete install_requirements.bat

--- install_requirements.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d %~dp0
-
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
-
-echo Installation complete. Now you can open "main.py" for open the game
-pause
